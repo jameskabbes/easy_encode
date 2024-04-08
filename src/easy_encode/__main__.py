c7afba794954fc58c9c2715d2c274b912de16e9d
James Kabbes <james.kabbes@gmail.com>
2024-04-08 16:33:29 -0500

    building type processign

--- src/easy_encode/__main__.py
+++ src/easy_encode/__main__.py
@@ -1,4 +1,4 @@
-from easy_encode import encode_dataclass_object
+from easy_encode import encode_dataclass_object, type_processing
 from dataclasses import dataclass, field
 from typing import Set, NewType, TypedDict, Callable, Any, Union, Literal
 import datetime
@@ -17,6 +17,10 @@ class Division:
     name: Union[str, None] = field(default=None)
 
 
+"""
 division = Division(1, datetime.datetime.now(), set(
     [1, 2, 3, 4]), set([1.0, 2.0]), 'east')
 print(encode_dataclass_object(division, 'json'))
+"""
+
+type_processing.test()
--- src/easy_encode/type_processing.py
+++ src/easy_encode/type_processing.py
@@ -9,6 +9,47 @@ def find_supertype(given_type):
     return given_type
 
 
+def is_union_type(given_type) -> bool:
+    return len(typing.get_args(given_type)) > 1
+
+
+def is_value_of_type(value, given_type, check_contents: typing.Literal['one', 'all'] = 'one') -> bool:
+    """recursively check if value is of type: given_type"""
+
+    print(value)
+    print(given_type)
+
+    value_type = type(value)
+    print(value_type)
+
+    # strip down the user defined NewTypes
+    given_supertype = find_supertype(given_type)
+
+    # see if this is a nested dict, list, or tuple
+    given_supertype_origin = typing.get_origin(given_supertype)
+    if given_supertype_origin in (dict, list, tuple):
+        print('Nested type')
+        if given_supertype_origin == dict:
+            # check key
+            # check value
+            pass
+            return False
+
+        if given_supertype_origin == list or given_supertype_origin == tuple:
+            return False
+
+    # not a nested type
+    else:
+        print('Not a nested type')
+        given_supertype_args = typing.get_args(given_supertype)
+        if len(given_supertype_args) > 1:
+            print('union type')
+            return value_type in (find_supertype(arg) for arg in given_supertype_args)
+        else:
+            print('not union type')
+            return value_type == given_supertype
+
+
 def find_priority_type(given_type):
     """for types that are a Union of other types, return the first one"""
 
@@ -44,3 +85,12 @@ def find_root_type(given_type):
 
 def inspect_nested_type(given_type, given_value):
     pass
+
+
+def test():
+
+    print(is_value_of_type(1, int))
+    print(is_value_of_type(1, float))
+    print(is_value_of_type([], list))
+    print(is_value_of_type((), tuple))
+    print(is_value_of_type([], list))
